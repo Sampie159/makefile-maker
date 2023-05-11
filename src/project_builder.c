ceeb3d03b853bbc299547705c258c2cc92b73898
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2023-05-10 23:55:27 -0300

    Code refactoring v6?

--- src/project_builder.c
+++ src/project_builder.c
@@ -41,76 +41,34 @@ void setup_files(const char *language, const char *project_name,
 
   FILE *makefile = fopen(makefile_path, "w");
 
-  if (strcmp(language, "c") == 0) {
-    fprintf(makefile,
-            "CFLAGS=-Wall -Wextra -g\n"
-            "CC = gcc\n\n"
-            "SRCDIR=src\n"
-            "SRCS=${wildcard $(SRCDIR)/*.c}\n\n"
-            "OBJDIR=obj\n"
-            "OBJS=${patsubst $(SRCDIR)/%%.c, $(OBJDIR)/%%.o, $(SRCS)}\n\n"
-            "BINDIR=bin\n\n"
-            "all: $(BINDIR)/%s\n\n"
-            "release: CFLAGS=-Wall -Wextra -DNDEBUG -pipe -march=native -O2\n"
-            "release: $(BINDIR)/%s\n\n"
-            "run: $(BINDIR)/%s\n"
-            "\t$(BINDIR)/%s\n\n"
-            "$(OBJDIR)/%%.o: $(SRCDIR)/%%.c\n"
-            "\t$(CC) $(CFLAGS) -c -o $@ $^\n\n"
-            "$(BINDIR)/%s: $(OBJS)\n"
-            "\t$(CC) $(CFLAGS) -o $@ $^\n\n"
-            "clean:\n"
-            "\trm $(OBJDIR)/*\n"
-            "\trm $(BINDIR)/%s",
-            project_name, project_name, project_name, project_name,
-            project_name, project_name);
-  } else if (strcmp(language, "cpp") == 0) {
-    fprintf(makefile,
-            "CFLAGS=-Wall -Wextra -g\n"
-            "CC = g++\n\n"
-            "SRCDIR=src\n"
-            "SRCS=${wildcard $(SRCDIR)/*.cpp}\n\n"
-            "OBJDIR=obj\n"
-            "OBJS=${patsubst $(SRCDIR)/%%.cpp, $(OBJDIR)/%%.o, $(SRCS)}\n\n"
-            "BINDIR=bin\n\n"
-            "all: $(BINDIR)/%s\n\n"
-            "release: CFLAGS=-Wall -Wextra -DNDEBUG -pipe -march=native -O2\n"
-            "release: $(BINDIR)/%s\n\n"
-            "run: $(BINDIR)/%s\n"
-            "\t$(BINDIR)/%s\n\n"
-            "$(OBJDIR)/%%.o: $(SRCDIR)/%%.cpp\n"
-            "\t$(CC) $(CFLAGS) -c -o $@ $^\n\n"
-            "$(BINDIR)/%s: $(OBJS)\n"
-            "\t$(CC) $(CFLAGS) -o $@ $^\n\n"
-            "clean:\n"
-            "\trm $(OBJDIR)/*\n"
-            "\trm $(BINDIR)/%s",
-            project_name, project_name, project_name, project_name,
-            project_name, project_name);
-  } else if (strcmp(language, "cc") == 0) {
-    fprintf(makefile,
-            "CFLAGS=-Wall -Wextra -g\n"
-            "CC = g++\n\n"
-            "SRCDIR=src\n"
-            "SRCS=${wildcard $(SRCDIR)/*.cc}\n\n"
-            "OBJDIR=obj\n"
-            "OBJS=${patsubst $(SRCDIR)/%%.cc, $(OBJDIR)/%%.o, $(SRCS)}\n\n"
-            "BINDIR=bin\n\n"
-            "all: $(BINDIR)/%s\n\n"
-            "release: CFLAGS=-Wall -Wextra -DNDEBUG -pipe -march=native -O2\n"
-            "release: $(BINDIR)/%s\n\n"
-            "run: $(BINDIR)/%s\n"
-            "\t$(BINDIR)/%s\n\n"
-            "$(OBJDIR)/%%.o: $(SRCDIR)/%%.cc\n"
-            "\t$(CC) $(CFLAGS) -c -o $@ $^\n\n"
-            "$(BINDIR)/%s: $(OBJS)\n"
-            "\t$(CC) $(CFLAGS) -o $@ $^\n\n"
-            "clean:\n"
-            "\trm $(OBJDIR)/*\n"
-            "\trm $(BINDIR)/%s",
-            project_name, project_name, project_name, project_name,
-            project_name, project_name);
-  }
+  char *compiler;
+  if (strcmp(language, "c") == 0)
+    compiler = "gcc";
+  else
+    compiler = "g++";
+
+  fprintf(makefile,
+          "CFLAGS=-Wall -Wextra -g\n"
+          "CC=%s\n\n"
+          "SRCDIR=src\n"
+          "SRCS=${wildcard $(SRCDIR)/*.%s}\n\n"
+          "OBJDIR=obj\n"
+          "OBJS=${patsubst $(SRCDIR)/%%.%s, $(OBJDIR)/%%.o, $(SRCS)}\n\n"
+          "BINDIR=bin\n\n"
+          "all: $(BINDIR)/%s\n\n"
+          "release: CFLAGS=-Wall -Wextra -DNDEBUG -pipe -march=native -O2\n"
+          "release: $(BINDIR)/%s\n\n"
+          "run: $(BINDIR)/%s\n"
+          "\t$(BINDIR)/%s\n\n"
+          "$(OBJDIR)/%%.o: $(SRCDIR)/%%.%s\n"
+          "\t$(CC) $(CFLAGS) -c -o $@ $^\n\n"
+          "$(BINDIR)/%s: $(OBJS)\n"
+          "\t$(CC) $(CFLAGS) -o $@ $^\n\n"
+          "clean:\n"
+          "\trm $(OBJDIR)/*\n"
+          "\trm $(BINDIR)/%s",
+          compiler, language, language, project_name, project_name,
+          project_name, project_name, language, project_name, project_name);
 
   fclose(makefile);
 
@@ -128,22 +86,9 @@ void setup_files(const char *language, const char *project_name,
                        "}");
 
     fclose(main_file);
-  } else if (strcmp(language, "cpp") == 0) {
+  } else {
     char main_path[MAX_SIZE];
-    snprintf(main_path, MAX_SIZE, "%s/src/main.cpp", project_path);
-
-    FILE *main_file = fopen(main_path, "w");
-
-    fprintf(main_file, "#include <iostream>\n\n"
-                       "int main() {\n"
-                       "\tstd::cout << \"Hello World!\\n\";\n\n"
-                       "\treturn 0;\n"
-                       "}");
-
-    fclose(main_file);
-  } else if (strcmp(language, "cc") == 0) {
-    char main_path[MAX_SIZE];
-    snprintf(main_path, MAX_SIZE, "%s/src/main.cc", project_path);
+    snprintf(main_path, MAX_SIZE, "%s/src/main.%s", project_path, language);
 
     FILE *main_file = fopen(main_path, "w");
 
